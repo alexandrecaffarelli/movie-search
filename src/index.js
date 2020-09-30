19b9ee3b8c9646cee98675211f6506db0b2a82cb
Alexandre Caffarelli <58951646+alexandrecaffarelli@users.noreply.github.com>
2020-09-30 13:19:18 +0200

    Build: changes to babel and webpack

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import SearchMovies from "./components/SearchMovies"
+import App from "./App";
 
-class Main extends Component {
-    render() {
-        return (
-            <div className="container">
-                <h1 className="title"> React Movie Search </h1>
-                <SearchMovies />
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<Main />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
