4aef5686a41988e638e04c744b236193b1038e3a
Alexandre Caffarelli <58951646+alexandrecaffarelli@users.noreply.github.com>
2020-09-30 13:16:12 +0200

    Feat: create getMovie function

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,13 +1,30 @@
 import React from "react"
 
-function SearchMovies () {
+export default function SearchMovies() {
+
+    const getMovie = async (event) => {
+        event.preventDefault();
+        console.log("submitting");
+
+        const query = "Jurassic Park";
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=bf2450b2165880b9d1e1d8dfa58f2774&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        try {
+            const result = await fetch(url);
+            const data = await result.json();
+            console.log(data);
+        } catch(error) {   
+            console.error(error);
+        }
+
+    }
+
     return (
-        <form className="form"> 
+        <form className="form" onSubmit={getMovie}> 
             <label className="label" htmlFor="query"> Movie Name </label>
             <input className="input" type="text" name="query" placeholder="i.e. Scarface"></input>
             <button className="button" type="submit"> Search </button>
         </form>
     )
-}
 
-export default SearchMovies
+}
