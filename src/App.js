d9f9b14a99d5c317016854627b6c1d953c2e34d9
Alexandre Caffarelli <58951646+alexandrecaffarelli@users.noreply.github.com>
2020-09-30 19:46:38 +0200

    Style: add front page image to app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SearchMovies from "./components/SearchMovies";
 export default function App() {
     return (
         <div className="container">
+            <img className="movie-image" src="./img/movie.jpg"/>
             <h1 className="title"> React Movie Search </h1>
             <SearchMovies />
         </div>
