4f4ebf5a9ba8694464113aabc62e9f34a6c25612
Alexandre Caffarelli <58951646+alexandrecaffarelli@users.noreply.github.com>
2020-09-30 19:28:46 +0200

    Feat: complete SearchMovies component

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,30 +1,38 @@
-import React from "react"
+import React, { useState } from "react";
+import MovieCard from "./MovieCard";
 
 export default function SearchMovies() {
 
+    const [query, setQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+
     const getMovie = async (event) => {
         event.preventDefault();
-        console.log("submitting");
 
-        const query = "Jurassic Park";
         const url = `https://api.themoviedb.org/3/search/movie?api_key=bf2450b2165880b9d1e1d8dfa58f2774&language=en-US&query=${query}&page=1&include_adult=false`;
 
         try {
             const result = await fetch(url);
             const data = await result.json();
-            console.log(data);
+            setMovies(data.results);
         } catch(error) {   
             console.error(error);
         }
-
     }
 
     return (
-        <form className="form" onSubmit={getMovie}> 
-            <label className="label" htmlFor="query"> Movie Name </label>
-            <input className="input" type="text" name="query" placeholder="i.e. Scarface"></input>
-            <button className="button" type="submit"> Search </button>
-        </form>
+        <>
+            <form className="form" onSubmit={getMovie}> 
+                <label className="label" htmlFor="query"> Movie Name </label>
+                <input className="input" type="text" name="query" placeholder="i.e. Scarface" value={query} onChange={ (event) => {setQuery(event.target.value)}}/>
+                <button className="button" type="submit"> Search </button>
+            </form>
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </div>
+        </>
     )
 
 }
